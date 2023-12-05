5e5708a0a46ae2ab71cc26800f0691a9d653cbfb
Afif Raihan <116819025+AfifRollink@users.noreply.github.com>
2023-12-06 04:24:09 +0700

    Add files via upload
    halaman feedback dan catat makanan

---
+++ Dishdash/Dishdash-master/app/src/main/java/com/example/dishdash/ui/WelcomeActivity/CatatMakananActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dishdash.ui.WelcomeActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dishdash.R
+
+class CatatMakananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catat_makanan)
+    }
+}
---
+++ Dishdash/Dishdash-master/app/src/main/java/com/example/dishdash/ui/WelcomeActivity/MostActiveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dishdash.ui.WelcomeActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dishdash.R
+
+class MostActiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_most_active)
+    }
+}
